e5a1c5963133378fd8a588845dfd51ff63158f6a
oba <obatirou@gmail.com>
2025-03-28 20:07:37 +0100

    RCON

--- src/BlockBreakers/AES.sol
+++ src/BlockBreakers/AES.sol
@@ -3,15 +3,17 @@ pragma solidity ^0.8.19;
 
 // https://davidwong.fr/blockbreakers/aes.html
 
+import "forge-std/console.sol";
+
 contract AES {
     // @notice The s-box is a lookup table that maps each byte to its corresponding value
     bytes constant s_box =
         hex"637c777bf26b6fc53001672bfed7ab76ca82c97dfa5947f0add4a2af9ca472c0b7fd9326363ff7cc34a5e5f171d8311504c723c31896059a071280e2eb27b27509832c1a1b6e5aa0523bd6b329e32f8453d100ed20fcb15b6acbbe394a4c58cfd0efaafb434d338545f9027f503c9fa851a3408f929d38f5bcb6da2110fff3d2cd0c13ec5f974417c4a77e3d645d197360814fdc222a908846eeb814de5e0bdbe0323a0a4906245cc2d3ac629195e479e7c8376d8dd54ea96c56f4ea657aae08ba78252e1ca6b4c6e8dd741f4bbd8b8a703eb5664803f60e613557b986c11d9ee1f8981169d98e949b1e87e9ce5528df8ca1890dbfe6426841992d0fb054bb16";
 
     // @notice The round constant is a lookup table that maps each round to its corresponding value
-    // See AESUtils.py for explanation
-    bytes constant rcon =
-        hex"8D01020408102040801B366CD8AB4D9A2F5EBC63C697356AD4B37DFAEFC591372E4D3BD61C29F254A943366CC831D374E8CB8D01020408102040801B366CDAB4D9A2F5EBC63C697356AD4B37DFAEC5913972E4D3BD61C29F254A943366C831D3A74E8CB8D01020408102040801366CD8AB4D9A2F5EBC63C697356AD4B7DFAEFC5913972E4D3BD61C29F254A93366CC831D3A74E8CB8D0102040810240801B366CD8AB4D9A2F5EBC63C69736AD4B37DFAEFC5913972E4D3BD61C29254A943366CC831D3A74E8CB8D0102008102040801B366CD8AB4D9A2F5EBC6C697356AD4B37DFAEFC5913972E4D3B61C29F254A943366CC831D3A74E8CB";
+    // See AESUtils.py for explanation: calculate_r_con(256)
+    bytes constant r_con =
+        hex"8d01020408102040801b366cd8ab4d9a2f5ebc63c697356ad4b37dfaefc5913972e4d3bd61c29f254a943366cc831d3a74e8cb8d01020408102040801b366cd8ab4d9a2f5ebc63c697356ad4b37dfaefc5913972e4d3bd61c29f254a943366cc831d3a74e8cb8d01020408102040801b366cd8ab4d9a2f5ebc63c697356ad4b37dfaefc5913972e4d3bd61c29f254a943366cc831d3a74e8cb8d01020408102040801b366cd8ab4d9a2f5ebc63c697356ad4b37dfaefc5913972e4d3bd61c29f254a943366cc831d3a74e8cb8d01020408102040801b366cd8ab4d9a2f5ebc63c697356ad4b37dfaefc5913972e4d3bd61c29f254a943366cc831d3a74e8cb8d";
 
     /// @notice Rotate a 4-byte word left by 8 bits
     /// @param word The word to rotate
@@ -43,6 +45,7 @@ contract AES {
     /// @param round The round number
     /// @return The round constant
     function RCon(uint256 round) public pure returns (bytes4) {
-        return bytes4(uint32(uint8(rcon[round])) << 24);
+        uint32 rcon_byte = uint32(uint8(r_con[round]));
+        return bytes4(rcon_byte << 24);
     }
 }
--- test/DiffTestingAES.t.sol
+++ test/DiffTestingAES.t.sol
@@ -16,42 +16,55 @@ contract DiffTestingAES is Test {
         aes = new AES();
     }
 
-    function call_python(string memory func_name, bytes4 word) public returns (bytes4) {
-        bytes memory word_input = abi.encode(word);
+    function call_python(string memory func_name, bytes memory input) public returns (bytes memory) {
         string[] memory inputs = new string[](4);
         inputs[0] = "python";
         inputs[1] = "test/AES.py";
         inputs[2] = func_name;
-        inputs[3] = word_input.toHexString();
+        inputs[3] = input.toHexString();
         bytes memory result = vm.ffi(inputs);
-        return abi.decode(result, (bytes4));
+        return result;
     }
 
-    function call_rust(string memory func_name, bytes4 word) public returns (bytes4) {
-        bytes memory word_input = abi.encode(word);
+    function call_rust(string memory func_name, bytes memory input) public returns (bytes memory) {
         string[] memory inputs = new string[](3);
         inputs[0] = "rust/target/release/rust";
         inputs[1] = func_name;
-        inputs[2] = word_input.toHexString();
+        inputs[2] = input.toHexString();
         bytes memory result = vm.ffi(inputs);
-        return abi.decode(result, (bytes4));
+        return result;
     }
 
     function testFuzz_RotWord(bytes4 word) public {
         bytes4 solidity_RotWord = aes.RotWord(word);
-        bytes4 python_RotWord = call_python("RotWord", word);
-        bytes4 rust_RotWord = call_rust("RotWord", word);
+        bytes memory python_RotWord = call_python("RotWord", abi.encode(word));
+        bytes memory rust_RotWord = call_rust("RotWord", abi.encode(word));
 
-        assertEq(solidity_RotWord, python_RotWord);
-        assertEq(solidity_RotWord, rust_RotWord);
+        assertEq(solidity_RotWord, abi.decode(python_RotWord, (bytes4)));
+        assertEq(solidity_RotWord, abi.decode(rust_RotWord, (bytes4)));
     }
 
     function testFuzz_SubWord(bytes4 word) public {
         bytes4 solidity_SubWord = aes.SubWord(word);
-        bytes4 python_SubWord = call_python("SubWord", word);
-        bytes4 rust_SubWord = call_rust("SubWord", word);
+        bytes memory python_SubWord = call_python("SubWord", abi.encode(word));
+        bytes memory rust_SubWord = call_rust("SubWord", abi.encode(word));
 
-        assertEq(solidity_SubWord, python_SubWord);
-        assertEq(solidity_SubWord, rust_SubWord);
+        assertEq(solidity_SubWord, abi.decode(python_SubWord, (bytes4)));
+        assertEq(solidity_SubWord, abi.decode(rust_SubWord, (bytes4)));
+    }
+
+    function testFuzz_RCon(uint256 round) public {
+        round = bound(round, 0, 255);
+        bytes4 solidity_RCon = aes.RCon(round);
+        for (uint256 i = 1; i < 4; i++) {
+            assertEq(solidity_RCon[i], 0);
+        }
+
+        bytes memory python_RCon = call_python("RCon", abi.encode(round));
+        bytes memory rust_RCon = call_rust("RCon", abi.encode(round));
+
+        uint256 solidity_res = uint256(uint8(solidity_RCon[0]));
+        assertEq(solidity_res, abi.decode(python_RCon, (uint256)));
+        assertEq(solidity_res, abi.decode(rust_RCon, (uint256)));
     }
 }
